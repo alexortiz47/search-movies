60f5490d1e633392722c70f4faa43349d51d5c88
Alejandro Ortiz <ortizalex.47@gmail.com>
2021-05-11 16:31:44 +0200

    Creación página 404

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Detail } from './pages/Detail';
 import { Home } from './pages/Home';
+import { Detail } from './pages/Detail';
+import { NotFound } from './pages/NotFound';
+
 
 import './App.css';
 import 'bulma/css/bulma.css';
@@ -13,6 +15,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route path='/detail/:movieId' component={Detail}/>
+          <Route component={NotFound}/>
         </Switch>
       </div>
     );
