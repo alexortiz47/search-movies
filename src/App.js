e1d643a571a816e998cba1a32fd7bca251890877
Alejandro Ortiz <ortizalex.47@gmail.com>
2021-05-11 12:47:41 +0200

    Fetch para obtener resultados y renderizado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import './App.css';
-import 'bulma/css/bulma.css';
+import React, { Component } from 'react';
 import { Title } from './components/Title';
 import { SearchForm } from './components/SearchForm';
 
-function App() {
-  return (
-    <div className="App">
-      <Title>Search Movies</Title>
-      <div className="search-form-wrapper">
-        <SearchForm></SearchForm>
+import './App.css';
+import 'bulma/css/bulma.css';
+
+class App extends Component {
+  state = { results: [] }
+
+  _handleResults = (results) => {
+    this.setState({ results }); 
+  }
+
+  _renderResults() {
+    const { results } = this.state;
+    return results.map(movie => {
+        return <p key={movie.imdbID}>{movie.Title}</p>
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title>Search Movies</Title>
+        <div className="search-form-wrapper">
+          <SearchForm onResults={this._handleResults}/>
+        </div>
+        {this.state.results.length === 0
+          ? <p>Sin resultados</p>
+          : this._renderResults()
+          }
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const API_KEY = '6f421a98';
+
 export class SearchForm extends Component {
     // inicializamos el state a vacío, donde iremos guardando lo que escribamos en el input (actualizando el state)
     state = {
@@ -12,15 +14,25 @@ export class SearchForm extends Component {
 
     _handleSubmit = (event) => {
         event.preventDefault();
-        alert(this.state.inputMovie);
+        const { inputMovie } = this.state;
+
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}
+        `)
+            .then(res => res.json())
+            .then(results => {
+                const { Search, totalResults } = results;
+                console.log({ Search, totalResults });
+                this.props.onResults(Search); // ejecutamos el método onResults que es una prop para pasarle al padre los resultados
+            })
     }
 
     render() {
         return (
+            // el navegador coge por defecto el último <button> que se encuentre para desencadenar el submit
             <form onSubmit={this._handleSubmit}>
                 <div className="field has-addons">
                     <div className="control">
-                        <input className="input" type="text" placeholder="Movie to search"
+                        <input className="input" type="text" placeholder="Movie to search..."
                         onChange={this._handleChange}/>
                     </div>
                     <div className="control">
